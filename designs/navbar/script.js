368697c403d9eb40846f37e9fbfdc7c4a4476e32
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-08-13 21:09:18 -0500

    more pushes

--- designs/navbar/script.js
+++ designs/navbar/script.js
@@ -2,11 +2,11 @@ function toggleNavbar() {
     const navbar = document.getElementById("navbar");
     const main = document.getElementById("main");
 
-    if (navbar.style.width === "250px") {
+    if (navbar.style.width === "250px" || navbar.style.width === "100%") {
         navbar.style.width = "0";
         main.style.marginLeft = "0";
     } else {
-        navbar.style.width = "250px";
-        main.style.marginLeft = "250px";
+        navbar.style.width = window.innerWidth <= 600 ? "100%" : "250px";
+        main.style.marginLeft = window.innerWidth <= 600 ? "0" : "250px";
     }
 }

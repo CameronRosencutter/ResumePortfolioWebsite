587d3db618f28a8db462278cabcc51fcf7000a91
Cameron Rosencutter <cameronrosencutter@yahoo.com>
2024-08-18 14:30:38 -0500

    pusssh

--- designs/temporary/script.js
+++ designs/temporary/script.js
@@ -1,12 +1,15 @@
 function toggleNavbar() {
     const navbar = document.getElementById("navbar");
     const main = document.getElementById("main");
+    const openbtn = document.querySelector(".openbtn");
 
     if (navbar.style.width === "250px" || navbar.style.width === "100%") {
         navbar.style.width = "0";
         main.style.marginLeft = "0";
+        openbtn.classList.remove("hidden");
     } else {
         navbar.style.width = window.innerWidth <= 600 ? "100%" : "250px";
         main.style.marginLeft = window.innerWidth <= 600 ? "0" : "250px";
+        openbtn.classList.add("hidden");
     }
 }
